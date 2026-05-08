4ff28743402b9de48c9d441ad918adbacd49df4f
umayer10-AI <mdumayer10@gmail.com>
2026-05-08 15:55:09 +0600

    add

--- app/src/app/user/page.jsx
+++ app/src/app/user/page.jsx
@@ -1,3 +1,4 @@
+import ModalTask from '@/component/ModalTask';
 import TableTask from '@/component/TableTask';
 import { deleteUser } from '@/lib/action';
 import { getUser } from '@/lib/data';
@@ -9,9 +10,9 @@ const page = async () => {
 
     return (
         <div>
-            <div>
+            <div className='flex justify-center gap-2 items-center my-5'>
                 <h2>Data: {data.length}</h2>
-                
+                <ModalTask></ModalTask>
             </div>
             <TableTask p={data} deleteUser={deleteUser}></TableTask>
         </div>
--- app/src/lib/action.js
+++ app/src/lib/action.js
@@ -2,6 +2,25 @@
 
 import { revalidatePath } from "next/cache"
 
+export const createUser = async (formData) => {
+
+    const newUser = Object.fromEntries(formData.entries())
+    console.log(newUser)
+
+    // const res = await fetch(`http://localhost:5000/user`,{
+    //     method: "POST",
+    //     headers: {
+    //         "content-type": "application/json"
+    //     },
+    //     body: JSON.stringify(newUser)
+    // })
+    // const data = await res.json()
+    // // if(data.deletedCount > 0){
+    // //     revalidatePath("/user")
+    // // }
+    // return data
+}
+
 export const deleteUser = async (id) => {
     const res = await fetch(`http://localhost:5000/user/${id}`,{
         method: "DELETE"
