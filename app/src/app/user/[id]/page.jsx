f79613200217f248fd8f98a0aac048fb2e206871
umayer10-AI <mdumayer10@gmail.com>
2026-05-08 21:42:47 +0600

    add

--- app/src/app/user/[id]/page.jsx
+++ app/src/app/user/[id]/page.jsx
@@ -1,18 +1,17 @@
-import { getId } from '@/lib/data';
-import React from 'react';
+import { getId } from "@/lib/data";
+import React from "react";
 
-const page = async ({params}) => {
+const page = async ({ params }) => {
+  const { id } = await params;
+  const data = await getId(id);
 
-    const {id} = await params
-    const data = await getId(id)
-
-    return (
-        <div className='rounded-xl shadow-lg space-y-2 shadow-cyan-500 w-fit p-5 m-10'>
-            <h2>Name: {data.name}</h2>
-            <h2>Email: {data.email}</h2>
-            <h2>Role: {data.role}</h2>
-        </div>
-    );
+  return (
+    <div className="rounded-xl shadow-lg space-y-2 shadow-cyan-500 w-fit p-5 m-10">
+      <h2>Name: {data.name}</h2>
+      <h2>Email: {data.email}</h2>
+      <h2>Role: {data.role}</h2>
+    </div>
+  );
 };
 
 export default page;
