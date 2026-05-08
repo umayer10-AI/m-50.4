c6ce403eea61e4b4c121b612c0ef1ba4773279df
umayer10-AI <mdumayer10@gmail.com>
2026-05-08 21:42:12 +0600

    add

--- app/src/lib/action.js
+++ app/src/lib/action.js
@@ -1,55 +1,53 @@
-"use server"
+"use server";
 
-import { revalidatePath } from "next/cache"
-import { redirect } from "next/navigation"
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
 
 export const createUser = async (formData) => {
+  const newUser = Object.fromEntries(formData.entries());
 
-    const newUser = Object.fromEntries(formData.entries())
+  const res = await fetch(`http://localhost:5000/user`, {
+    method: "POST",
+    headers: {
+      "content-type": "application/json",
+    },
+    body: JSON.stringify(newUser),
+  });
+  const data = await res.json();
+  if (data.insertedId) {
+    revalidatePath("/user");
+  }
+  console.log(data);
+  return data;
+};
 
-    const res = await fetch(`http://localhost:5000/user`,{
-        method: "POST",
-        headers: {
-            "content-type": "application/json"
-        },
-        body: JSON.stringify(newUser)
-    })
-    const data = await res.json()
-    if(data.insertedId){
-        revalidatePath("/user")
-    }
-    console.log(data)
-    return data
-}
+export const updateUser = async (id, formData) => {
+  const newUser = Object.fromEntries(formData.entries());
+  console.log(newUser);
 
-export const updateUser = async (id,formData) => {
-
-    const newUser = Object.fromEntries(formData.entries())
-    console.log(newUser)
-
-    const res = await fetch(`http://localhost:5000/user/${id}`,{
-        method: "PATCH",
-        headers: {
-            "content-type": "application/json"
-        },
-        body: JSON.stringify(newUser)
-    })
-    const data = await res.json()
-    if(data.modifiedCount > 0){
-        revalidatePath("/user")
-        redirect("/user")
-    }
-    console.log(data)
-    return data
-}
+  const res = await fetch(`http://localhost:5000/user/${id}`, {
+    method: "PATCH",
+    headers: {
+      "content-type": "application/json",
+    },
+    body: JSON.stringify(newUser),
+  });
+  const data = await res.json();
+  if (data.modifiedCount > 0) {
+    revalidatePath("/user");
+    redirect("/user");
+  }
+  console.log(data);
+  return data;
+};
 
 export const deleteUser = async (id) => {
-    const res = await fetch(`http://localhost:5000/user/${id}`,{
-        method: "DELETE"
-    })
-    const data = await res.json()
-    if(data.deletedCount > 0){
-        revalidatePath("/user")
-    }
-    return data
-}
+  const res = await fetch(`http://localhost:5000/user/${id}`, {
+    method: "DELETE",
+  });
+  const data = await res.json();
+  if (data.deletedCount > 0) {
+    revalidatePath("/user");
+  }
+  return data;
+};
