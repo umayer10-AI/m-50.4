ead040d97239d7eb032d54f8f645aea1dd2b5250
umayer10-AI <mdumayer10@gmail.com>
2026-05-08 21:42:59 +0600

    add

--- app/src/app/user/page.jsx
+++ app/src/app/user/page.jsx
@@ -1,22 +1,21 @@
-import ModalTask from '@/component/ModalTask';
-import TableTask from '@/component/TableTask';
-import { deleteUser } from '@/lib/action';
-import { getUser } from '@/lib/data';
-import React from 'react';
+import ModalTask from "@/component/ModalTask";
+import TableTask from "@/component/TableTask";
+import { deleteUser } from "@/lib/action";
+import { getUser } from "@/lib/data";
+import React from "react";
 
 const page = async () => {
+  const data = await getUser();
 
-    const data = await getUser()
-
-    return (
-        <div>
-            <div className='flex justify-center gap-2 items-center my-5'>
-                <h2>Data: {data.length}</h2>
-                <ModalTask></ModalTask>
-            </div>
-            <TableTask p={data} deleteUser={deleteUser}></TableTask>
-        </div>
-    );
+  return (
+    <div>
+      <div className="flex justify-center gap-2 items-center my-5">
+        <h2>Data: {data.length}</h2>
+        <ModalTask></ModalTask>
+      </div>
+      <TableTask p={data} deleteUser={deleteUser}></TableTask>
+    </div>
+  );
 };
 
 export default page;
