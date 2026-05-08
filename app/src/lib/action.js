4e408893fb4f6eb5f2e5e214aa78ee8c5cdb5986
umayer10-AI <mdumayer10@gmail.com>
2026-05-08 15:50:13 +0600

    add

--- app/src/lib/action.js
+++ app/src/lib/action.js
@@ -1,12 +1,14 @@
 "use server"
 
+import { revalidatePath } from "next/cache"
+
 export const deleteUser = async (id) => {
     const res = await fetch(`http://localhost:5000/user/${id}`,{
         method: "DELETE"
     })
     const data = await res.json()
     if(data.deletedCount > 0){
-        
+        revalidatePath("/user")
     }
     return data
 }
