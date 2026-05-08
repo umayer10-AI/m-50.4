1498c2048ca26960dd1fbf55f4db36b2086d4f14
umayer10-AI <mdumayer10@gmail.com>
2026-05-08 21:41:58 +0600

    add

--- app/src/app/user/[id]/edit/page.jsx
+++ app/src/app/user/[id]/edit/page.jsx
@@ -1,47 +1,64 @@
-import { updateUser } from '@/lib/action';
-import { getId } from '@/lib/data';
-import { Button, Input, Label, TextField } from '@heroui/react';
-import React from 'react';
+import { updateUser } from "@/lib/action";
+import { getId } from "@/lib/data";
+import { Button, Input, Label, TextField } from "@heroui/react";
+import React from "react";
 
-const page = async ({params}) => {
+const page = async ({ params }) => {
+  const { id } = await params;
+  const data = await getId(id);
 
-    const {id} = await params
-    const data = await getId(id)
+  const a = async (formData) => {
+    "use server";
+    await updateUser(id, formData);
+  };
 
-    const a = async (formData) => {
-        'use server'
-        await updateUser(id,formData)
-    }
+  return (
+    <div className="w-1/2 mx-auto rounded-xl shadow-lg shadow-cyan-600 p-5 border border-cyan-600 mt-10">
+      <h2 className="text-cyan-400 font-bold my-5">
+        Editing Site: {data.name}
+      </h2>
+      <div>
+        <form action={a} className="flex flex-col gap-4">
+          <TextField
+            className="w-full"
+            name="name"
+            defaultValue={data?.name}
+            type="text"
+          >
+            <Label>Name</Label>
+            <Input placeholder="Enter your name" />
+          </TextField>
+          <TextField
+            className="w-full"
+            name="email"
+            defaultValue={data?.email}
+            type="email"
+          >
+            <Label>Email</Label>
+            <Input placeholder="Enter your email" />
+          </TextField>
+          <TextField
+            className="w-full"
+            name="role"
+            defaultValue={data?.role}
+            type="text"
+          >
+            <Label>Role</Label>
+            <Input placeholder="Enter your role" />
+          </TextField>
 
-    return (
-        <div className='w-1/2 mx-auto rounded-xl shadow-lg shadow-cyan-600 p-5 border border-cyan-600 mt-10'>
-            <h2 className='text-cyan-400 font-bold my-5'>Editing Site: {data.name}</h2>
-            <div>
-                <form action={a} className="flex flex-col gap-4">
-                                  <TextField className="w-full" name="name" defaultValue={data?.name} type="text">
-                                    <Label>Name</Label>
-                                    <Input placeholder="Enter your name" />
-                                  </TextField>
-                                  <TextField className="w-full" name="email" defaultValue={data?.email} type="email">
-                                    <Label>Email</Label>
-                                    <Input placeholder="Enter your email" />
-                                  </TextField>
-                                  <TextField className="w-full" name="role" defaultValue={data?.role} type="text">
-                                    <Label>Role</Label>
-                                    <Input placeholder="Enter your role" />
-                                  </TextField>
-                
-                              <div className='flex items-center gap-2'>
-                                <Button slot="close" variant="secondary">
-                                Cancel
-                              </Button>
-                              <Button type='submit' slot="close">Update</Button>
-                              </div>
-                
-                                </form>
-            </div>
-        </div>
-    );
+          <div className="flex items-center gap-2">
+            <Button slot="close" variant="secondary">
+              Cancel
+            </Button>
+            <Button type="submit" slot="close">
+              Update
+            </Button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default page;
