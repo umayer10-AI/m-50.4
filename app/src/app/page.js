010fcc2b86b5cdf985d641471495ba1f1d18dc79
umayer10-AI <mdumayer10@gmail.com>
2026-05-08 15:25:44 +0600

    add

--- app/src/app/page.js
+++ app/src/app/page.js
@@ -3,7 +3,7 @@ import React from 'react';
 const page = () => {
   return (
     <div>
-      
+      <h2 className='text-3xl font-bold text-center my-10'>Umayer Home Page</h2>
     </div>
   );
 };
