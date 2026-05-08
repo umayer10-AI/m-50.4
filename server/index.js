1fff265b7e7ab6fac8c6508026725966ab05e8fa
umayer10-AI <mdumayer10@gmail.com>
2026-05-08 15:21:49 +0600

    add

--- server/index.js
+++ server/index.js
@@ -1,10 +1,44 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000
 
 app.use(cors())
 app.use(express.json())
+
+const uri = "mongodb+srv://server6:hmS5qzFwJvCMOtKC@cluster0.ywso1qs.mongodb.net/?appName=Cluster0";
+
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+const run = async () => {
+    try {
+        await client.connect();
+
+        const db = client.db("simpleClud5")
+        const userCollection = await db.collection("users")
+
+        app.get("/user", async (req,res) => {
+            const cursor = await userCollection.find()
+            const result = await cursor.toArray()
+            res.send(result)
+        })
+
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } 
+    finally {
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
 
 app.get('/', (req, res) => {
   res.send('Hello World Umayer')
