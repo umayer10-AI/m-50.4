87c0ddacfbd8b428d9c267f492e7e696d3e2c6ef
umayer10-AI <mdumayer10@gmail.com>
2026-05-08 15:59:15 +0600

    add

--- app/src/component/ModalTask.jsx
+++ app/src/component/ModalTask.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import {Envelope} from "@gravity-ui/icons";
 import {Button, Input, Label, Modal, Surface, TextField} from "@heroui/react";
+import { createUser } from '@/lib/action';
 
 const ModalTask = () => {
     return (
         <div>
             <Modal>
-      <Button variant="secondary">Open Contact Form</Button>
+      <Button variant="secondary">Add User</Button>
       <Modal.Backdrop>
         <Modal.Container placement="auto">
           <Modal.Dialog className="sm:max-w-md">
@@ -15,15 +16,12 @@ const ModalTask = () => {
               <Modal.Icon className="bg-accent-soft text-accent-soft-foreground">
                 <Envelope className="size-5" />
               </Modal.Icon>
-              <Modal.Heading>Contact Us</Modal.Heading>
-              <p className="mt-1.5 text-sm leading-5 text-muted">
-                Fill out the form below and get back to you. The modal adapts automatically
-                when the keyboard appears on mobile.
-              </p>
+              <Modal.Heading>Add User</Modal.Heading>
+
             </Modal.Header>
             <Modal.Body className="p-6">
               <Surface variant="default">
-                <form className="flex flex-col gap-4">
+                <form action={createUser} className="flex flex-col gap-4">
                   <TextField className="w-full" name="name" type="text">
                     <Label>Name</Label>
                     <Input placeholder="Enter your name" />
@@ -32,27 +30,21 @@ const ModalTask = () => {
                     <Label>Email</Label>
                     <Input placeholder="Enter your email" />
                   </TextField>
-                  <TextField className="w-full" name="phone" type="tel">
-                    <Label>Phone</Label>
-                    <Input placeholder="Enter your phone number" />
+                  <TextField className="w-full" name="role" type="text">
+                    <Label>Role</Label>
+                    <Input placeholder="Enter your role" />
                   </TextField>
-                  <TextField className="w-full" name="company">
-                    <Label>Company</Label>
-                    <Input placeholder="Enter your company name" />
-                  </TextField>
-                  <TextField className="w-full" name="message">
-                    <Label>Message</Label>
-                    <Input placeholder="Enter your message" />
-                  </TextField>
-                </form>
-              </Surface>
-            </Modal.Body>
+
             <Modal.Footer>
               <Button slot="close" variant="secondary">
                 Cancel
               </Button>
-              <Button slot="close">Send Message</Button>
+              <Button type='submit' slot="close">Add</Button>
             </Modal.Footer>
+
+                </form>
+              </Surface>
+            </Modal.Body>
           </Modal.Dialog>
         </Modal.Container>
       </Modal.Backdrop>
--- app/src/lib/action.js
+++ app/src/lib/action.js
@@ -7,18 +7,18 @@ export const createUser = async (formData) => {
     const newUser = Object.fromEntries(formData.entries())
     console.log(newUser)
 
-    // const res = await fetch(`http://localhost:5000/user`,{
-    //     method: "POST",
-    //     headers: {
-    //         "content-type": "application/json"
-    //     },
-    //     body: JSON.stringify(newUser)
-    // })
-    // const data = await res.json()
-    // // if(data.deletedCount > 0){
-    // //     revalidatePath("/user")
-    // // }
-    // return data
+    const res = await fetch(`http://localhost:5000/user`,{
+        method: "POST",
+        headers: {
+            "content-type": "application/json"
+        },
+        body: JSON.stringify(newUser)
+    })
+    const data = await res.json()
+    // if(data.deletedCount > 0){
+    //     revalidatePath("/user")
+    // }
+    return data
 }
 
 export const deleteUser = async (id) => {
